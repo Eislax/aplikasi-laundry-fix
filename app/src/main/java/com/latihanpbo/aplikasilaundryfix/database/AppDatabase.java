961a60acd4a03c8b430c3d816e7b6d8f05218b95
imron mustari <imronmustari214@gmail.com>
2023-07-14 21:51:43 +0700

    mengganti color

--- app/src/main/java/com/latihanpbo/aplikasilaundryfix/database/AppDatabase.java
+++ app/src/main/java/com/latihanpbo/aplikasilaundryfix/database/AppDatabase.java
@@ -6,14 +6,7 @@ import androidx.room.RoomDatabase;
 import com.latihanpbo.aplikasilaundryfix.database.dao.LaundryDao;
 import com.latihanpbo.aplikasilaundryfix.model.ModelLaundry;
 
-/**
- * Created by Azhar Rivaldi on 19-11-2021
- * Youtube Channel : https://bit.ly/2PJMowZ
- * Github : https://github.com/AzharRivaldi
- * Twitter : https://twitter.com/azharrvldi_
- * Instagram : https://www.instagram.com/azhardvls_
- * LinkedIn : https://www.linkedin.com/in/azhar-rivaldi
- */
+
 
 @Database(entities = {ModelLaundry.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
