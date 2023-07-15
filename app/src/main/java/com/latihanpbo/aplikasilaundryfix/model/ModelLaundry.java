08fa23a72132ce612d2bd0807562f6d5c1fec2ca
KILABID <aldimurad@gmail.com>
2023-07-15 17:31:12 +0700

    Add Splash Screen

--- app/src/main/java/com/latihanpbo/aplikasilaundryfix/model/ModelLaundry.java
+++ app/src/main/java/com/latihanpbo/aplikasilaundryfix/model/ModelLaundry.java
@@ -7,14 +7,6 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-/**
- * Created by Azhar Rivaldi on 19-11-2021
- * Youtube Channel : https://bit.ly/2PJMowZ
- * Github : https://github.com/AzharRivaldi
- * Twitter : https://twitter.com/azharrvldi_
- * Instagram : https://www.instagram.com/azhardvls_
- * LinkedIn : https://www.linkedin.com/in/azhar-rivaldi
- */
 
 @Entity(tableName = "tbl_laundry")
 public class ModelLaundry implements Parcelable {
--- app/src/main/java/com/latihanpbo/aplikasilaundryfix/networking/ApiInterface.java
+++ app/src/main/java/com/latihanpbo/aplikasilaundryfix/networking/ApiInterface.java
@@ -6,14 +6,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-/**
- * Created by Azhar Rivaldi on 19-11-2021
- * Youtube Channel : https://bit.ly/2PJMowZ
- * Github : https://github.com/AzharRivaldi
- * Twitter : https://twitter.com/azharrvldi_
- * Instagram : https://www.instagram.com/azhardvls_
- * LinkedIn : https://www.linkedin.com/in/azhar-rivaldi
- */
 
 public interface ApiInterface {
     @GET("place/nearbysearch/json")
